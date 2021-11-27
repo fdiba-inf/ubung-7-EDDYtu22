741f3d54df15edf484d156aeecd5dbf20f368a31
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2021-11-27 21:15:26 +0200

    Ubung 7

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -13,14 +13,22 @@ public class Triangle {
             printLine(1, line);
         }
 
-        // Print the bottom part of the triangle that is under the longest line
+        for (int line = n-1; line >= 1; line--) {
+            printLine(1, line - 1 );
+        }
+
     }
 
     public static void printLine(int start, int end) {
-        for (int i = start; i <= end; i++) {
-            System.out.print(i);
+        for (int i = start; i <= end; i++){
+            if(i == end){
+                System.out.print(i);
+            } else {
+                System.out.print(i + " ");
+            }
 
         }
+        System.out.println();
+
     }
 }
-
